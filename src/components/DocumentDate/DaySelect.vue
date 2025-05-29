8b53353462b0260adbb236de862931bf1353ea17
Michael98Arias <maicol98arias@gmail.com>
2025-05-28 19:06:58 -0500

    feat(document-date-page): add view, style, components store and functionality of the document date

---
+++ src/components/DocumentDate/DaySelect.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-select
+    dense
+    :error="!!error"
+    :error-messages="error"
+    :items="dayOptions"
+    :label="label"
+    :model-value="modelValue"
+    outlined
+    @update:model-value="$emit('update:modelValue', $event)"
+  />
+</template>
+<script setup lang="ts">
+  defineProps({
+    modelValue: Number,
+    dayOptions: Array,
+    label: String,
+    error: String,
+  });
+
+  defineEmits(['update:modelValue']);
+</script>
